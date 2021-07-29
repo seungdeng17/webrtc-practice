f8b07e1c3e3b53a8f0cd90968b50c3710db77fe5
이승택 <seungdeng17@gmail.com>
2021-07-29 22:16:09 +0900

    feat: https server

--- server/index.js
+++ server/index.js
@@ -1,29 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+const https = require("https");
 const express = require("express");
-const http = require("http");
+const credentials = {
+  key: fs.readFileSync("./cert/localhost.key"),
+  cert: fs.readFileSync("./cert/localhost.crt"),
+};
+
 const app = express();
-const server = http.createServer(app);
-const socket = require("socket.io");
-const io = socket(server);
+const httpsServer = https.createServer(credentials, app);
 
-const users = {};
-
-io.on('connection', socket => {
-    if (!users[socket.id]) {
-        users[socket.id] = socket.id;
-    }
-    socket.emit("yourID", socket.id);
-    io.sockets.emit("allUsers", users);
-    socket.on('disconnect', () => {
-        delete users[socket.id];
-    })
-
-    socket.on("callUser", (data) => {
-        io.to(data.userToCall).emit('hey', { signal: data.signalData, from: data.from });
-    })
-
-    socket.on("acceptCall", (data) => {
-        io.to(data.to).emit('callAccepted', data.signal);
-    })
+app.use(express.static("public"));
+app.use("/static", express.static(path.join(__dirname, "../client/build", "/static")));
+app.use("/asset-manifest.json", express.static(path.join(__dirname, "../client/build", "/asset-manifest.json")));
+app.get("/*", (_, res) => {
+  res.status(200).sendFile(path.join(__dirname, "../client/build", "/index.html"));
 });
 
-server.listen(8000, () => console.log('server is running on port 8000'));
+httpsServer.listen(3001, () => {
+  console.log("HTTPS Server is running at 3001!");
+});
