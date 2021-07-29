3e5dcbb7ab5fea4147b24253c0c5471a6238ec42
이승택 <seungdeng17@gmail.com>
2021-07-29 22:25:22 +0900

    feat: http redirect

--- server/index.js
+++ server/index.js
@@ -10,6 +10,14 @@ const credentials = {
 const app = express();
 const httpsServer = https.createServer(credentials, app);
 
+app.use((req, res, next) => {
+  if (!req.secure) {
+    res.redirect(`https://localhost:3002${req.url}`);
+  } else {
+    next();
+  }
+});
+
 app.use(express.static("public"));
 app.use("/static", express.static(path.join(__dirname, "../client/build", "/static")));
 app.use("/asset-manifest.json", express.static(path.join(__dirname, "../client/build", "/asset-manifest.json")));
@@ -17,6 +25,9 @@ app.get("/*", (_, res) => {
   res.status(200).sendFile(path.join(__dirname, "../client/build", "/index.html"));
 });
 
-httpsServer.listen(3001, () => {
-  console.log("HTTPS Server is running at 3001!");
+app.listen(3001, () => {
+  console.log("HTTP Server is running at 3001!");
+});
+httpsServer.listen(3002, () => {
+  console.log("HTTPS Server is running at 3002!");
 });
