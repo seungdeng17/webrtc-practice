7a2ec76d2ea44e80345e19a3c931f959b614b7c7
이승택 <seungdeng17@gmail.com>
2021-07-30 19:55:39 +0900

    feat: connect test

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
+const path = require("path");
+const http = require("http");
 const https = require("https");
 const express = require("express");
 const socket = require("socket.io");
+const cors = require('cors');
 
 // set https server
 const credentials = {
@@ -9,7 +12,19 @@ const credentials = {
   cert: fs.readFileSync("./cert/localhost.crt"),
 };
 const app = express();
+const httpServer = https.createServer(app);
 const httpsServer = https.createServer(credentials, app);
+
+app.use(cors({
+  origin: 'https://192.168.0.4:3000',
+  credentials: true,
+}));
+app.use(express.static("public"));
+app.use("/static", express.static(path.join(__dirname, "../client/build", "/static")));
+app.use("/asset-manifest.json", express.static(path.join(__dirname, "../client/build", "/asset-manifest.json")));
+app.get("/*", (_, res) => {
+  res.status(200).sendFile(path.join(__dirname, "../client/build", "/index.html"));
+});
 
 // socket io
 const io = socket(httpsServer);
@@ -42,6 +57,15 @@ io.on("connection", (socket) => {
 });
 
 // running server
-httpsServer.listen(443, () => {
-  console.log("HTTPS Server is running at 443!");
+// httpsServer.listen(443, () => {
+//   console.log("HTTPS Server is running at 443!");
+// });
+
+// httpsServer.listen(8000, () => {
+//   console.log("HTTP Server is running at 8000!");
+// });
+
+httpsServer.listen(8000, '192.168.0.4');
+httpsServer.on('listening', function() {
+    console.log('Express server started on port %s at %s', httpsServer.address().port, httpsServer.address().address);
 });
